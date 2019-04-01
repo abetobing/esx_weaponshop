0c9a0a4886f5af1ef74abf14912536ffc564248f
Abe Tobing <abedzilla@gmail.com>
2019-04-01 16:25:53 +0700

    Adding ped and vehicle into black market weapon

--- config.lua
+++ config.lua
@@ -2,13 +2,13 @@ Config               = {}
 
 Config.DrawDistance  = 100
 Config.Size          = { x = 1.5, y = 1.5, z = 0.5 }
-Config.Color         = { r = 0, g = 128, b = 255 }
+Config.Color         = { r = 255, g = 0, b = 255 }
 Config.Type          = 1
 
-Config.Locale        = 'fr'
+Config.Locale        = 'en'
 
-Config.LicenseEnable = false -- only turn this on if you are using esx_license
-Config.LicensePrice  = 5000
+Config.LicenseEnable = true -- only turn this on if you are using esx_license
+Config.LicensePrice  = 500000
 
 Config.Zones = {
 
@@ -25,14 +25,24 @@ Config.Zones = {
 			vector3(2567.6, 294.3, 107.7),
 			vector3(-1117.5, 2698.6, 17.5),
 			vector3(842.4, -1033.4, 27.1)
-		}
+		},
+		SellerPed = {},
+		Vehicles = {}
 	},
 
 	BlackWeashop = {
 		Legal = false,
 		Items = {},
 		Locations = {
-			vector3(-1306.2, -394.0, 35.6)
+			vector3(-1306.2, -394.0, 35.6),
+			vector3(342.29, -1211.05, 29.8),
+			vector3(174.34, -1830.01, 27.12)
+		},
+		SellerPed = {
+			{pedtype = 17, model = "csb_hao", x = 176.13, y = -1828.03, z = 28.13, heading = 150.64}
+		},
+		Vehicles = {
+			{model = "burrito", x = 176.94, y = -1832.77, z = 28.11, heading = 133.26}
 		}
 	}
 
